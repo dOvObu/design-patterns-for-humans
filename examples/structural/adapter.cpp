00d6c1fdf438ef71f6e760d1e8c187eb6a85ef29
Permyakov Mikhail <43134602+dOvObu@users.noreply.github.com>
2025-08-30 16:24:16 +0300

    Update adapter.cpp

--- examples/structural/adapter.cpp
+++ examples/structural/adapter.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 
-// например api opengl или directx
-struct adaptee
+// разные библиотеки, например api opengl и directx
+struct adaptee1
 {
   void log_int(int num)
   {
@@ -12,28 +12,15 @@ struct adaptee
 };
 
 
-// например разные адаптеры к api разных библиотек
-struct i_adapter
+// клиентский код, например движок
+struct i_target
 {
   virtual void print(std::string text) = 0;
 };
 
-struct adapter : public i_adapter
-{
-  void print(std::string text) override
-  {
-    if (!isdigit(text[0])) return;
-    logger.log_int(atoi(text.c_str()));
-  }
-private:
-  adaptee logger;
-};
-
-
-// например движок
 struct target
 {
-  i_adapter* logger = nullptr;
+  i_target* logger = nullptr;
   
   void update_with(std::string text)
   {
@@ -43,9 +30,22 @@ struct target
 };
 
 
+// разные адаптеры, например к api opengl и directx
+struct adapter : public i_target
+{
+  void print(std::string text) override
+  {
+    if (!isdigit(text[0])) return;
+    logger.log_int(atoi(text.c_str()));
+  }
+private:
+  adaptee1 logger;
+};
+
+
 int main()
 {
-  i_adapter* l = new adapter();
+  i_target* l = new adapter();
   
   target client;
   client.logger = l; // с подключением :)
