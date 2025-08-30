7f0ca08421a7352ec0acc2ce948d042619b56a9d
Permyakov Mikhail <43134602+dOvObu@users.noreply.github.com>
2025-08-30 16:09:32 +0300

    Create adapter.cpp

---
+++ examples/structural/adapter.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+
+struct adaptee
+{
+  void log_int(int num)
+  {
+    std::cout << "num: " << num << std::endl; 
+  }
+};
+
+struct i_adapter
+{
+  virtual void print(std::string text) = 0;
+};
+
+struct adapter : public i_adapter
+{
+  void print(std::string text) override
+  {
+    if (isdigit(text[0]))
+    {
+      logger.log_int(atoi(text.c_str()));
+    }
+  }
+private:
+  adaptee logger;
+};
+
+struct client
+{
+  i_adapter* logger = nullptr;
+  
+  void update_with(std::string text)
+  {
+    if (logger == nullptr) return;
+    logger->print(text);
+  }
+};
+
+
+int main()
+{
+  i_adapter* l = new adapter();
+  client cl;
+  
+  cl.logger = l;
+  cl.update_with("42");
+
+  return 0;
+}
